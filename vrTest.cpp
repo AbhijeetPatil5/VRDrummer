329252ea75d570f2a73ce47fad7b99657c1849c9
Abhijeet Patil <abhijeetpatil45@gmail.com>
2026-06-21 22:06:00 -0700

    vrTest.cpp now correctly detects Meta Quest 3S

--- vrTest.cpp
+++ vrTest.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string_view>
+#include <vector>
 #include <openxr/openxr.h>
 
 int main() {
@@ -16,15 +17,52 @@ int main() {
     appInfo.engineVersion = enginerVersion;
     appInfo.apiVersion = XR_CURRENT_API_VERSION;
 
+    const char* requestedExtensions[] = {
+        "XR_FB_passthrough"
+    };    
+
     XrInstanceCreateInfo createInfo = {};
     createInfo.type = XR_TYPE_INSTANCE_CREATE_INFO;
     createInfo.applicationInfo = appInfo;
+    createInfo.enabledExtensionCount = 1; 
+    createInfo.enabledExtensionNames = requestedExtensions;    
 
     XrInstance instance = XR_NULL_HANDLE;
     XrResult result = xrCreateInstance(&createInfo, &instance);
 
     if (XR_SUCCEEDED(result)) {
         std::cout << "SUCEESS: OpenXR Instance created. Connected to runtine!" << std::endl;
+
+        XrSystemGetInfo systemInfo = {};
+        systemInfo.type = XR_TYPE_SYSTEM_GET_INFO;
+        systemInfo.formFactor = XR_FORM_FACTOR_HEAD_MOUNTED_DISPLAY;
+
+        XrSystemId systemId;
+        XrResult sysResult = xrGetSystem(instance, &systemInfo, &systemId);
+
+        if (XR_SUCCEEDED(sysResult)) {
+            std::cout << "SUCCESS: Found VR Headset. System ID: " << systemId << std::endl;
+
+
+            // --- STEP 3: LOAD PROPRIETARY EXTENSION FUNCTIONS ---
+            PFN_xrCreatePassthroughFB pfnCreatePassthroughFB = nullptr;
+            
+            XrResult procResult = xrGetInstanceProcAddr(
+                instance, 
+                "xrCreatePassthroughFB", 
+                (PFN_xrVoidFunction*)&pfnCreatePassthroughFB
+            );
+
+            if (XR_SUCCEEDED(procResult) && pfnCreatePassthroughFB != nullptr) {
+                std::cout << "SUCCESS: Meta Passthrough API successfully unlocked and loaded!" << std::endl;
+            } else {
+                std::cout << "FAILED: Extension accepted, but could not load function pointers." << std::endl;
+            }
+
+        } else {
+            std::cout << "FAILED: Could not find a connected headset." << std::endl;
+        }
+
         xrDestroyInstance(instance);
     } else {
         std::cout << "FAILED: Could not create OpenXR Instance. Error code: " << result << std::endl;
